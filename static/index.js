f564bf0d5458da904568ea50b74f74dc738c379c
Ty Day <tyrday@gmail.com>
2018-09-24 13:32:15 -0400

    basic setup of page/js/application

---
+++ static/index.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Connect to websocket
+    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
+    
+})
