efd5ba775edd03fbd06dd7ad198defcd3292e132
Ty Day <tyrday@gmail.com>
2018-09-26 22:37:21 -0400

    add: rudimentary chat feature

--- application.py
+++ application.py
@@ -8,6 +8,8 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = 'secret!'
 socketio = SocketIO(app)
 
+chat_rooms = {0:{"name":"general","chat_log":[]},1:{"name":"hobbies","chat_log":[]}}
+
 
 @app.route("/")
 def index():
@@ -18,5 +20,14 @@ def vote(data):
     selection = data["selection"]
     emit("announce vote", {"selection": selection}, broadcast=True)
 
+@socketio.on("submit chat")
+def chat(data):
+    print(data)
+    chat_room = data["chat_room"]
+    text = data["chat_text"]
+    print(text)
+    chat_rooms[chat_room]["chat_log"] = text
+    emit("announce chat", {"chat_text": text}, broadcast=True)
+    
 if __name__=='__main__':
     socketio.run(app)
--- static/index.js
+++ static/index.js
@@ -1,7 +1,41 @@
+let counter = 0;
 document.addEventListener('DOMContentLoaded', () => {
 
     // Connect to websocket
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
+    socket.on('connect', () => {
+        document.querySelector('button').onclick = count;
+        // document.querySelector('#chat_box').onsubmit = submit_chat(socket);
+        document.querySelector('#chat_box').onsubmit = () => {
+            chat_text = document.querySelector('#chat_input').value;
+            console.log(chat_text);
+            chat_room = 0;
+            socket.emit('submit chat', {'chat_room':chat_room,'chat_text':chat_text});
+            document.querySelector('#chat_input').value = "";
+            return false;
+        }
+    })
+
+    socket.on('announce chat', data => {
+        const li = document.createElement('li')
+        li.innerHTML = data['chat_text']
+        document.querySelector('#chat_window_list').appendChild(li)
+        // document.querySelector('h1').innerHTML = data['chat_text']
+    })
+
     
 })
+
+function count() {
+    counter ++;
+    document.querySelector('h1').innerHTML = counter;
+}
+
+function submit_chat(x){
+    chat_text = document.querySelector('#chat_input').value
+    console.log(chat_text)
+    chat_room = 0
+    x.emit('submit chat', {'chat_room':chat_room,'chat_text':chat_text})
+    return false;
+}
