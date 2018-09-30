00ad0147a3a3ffe77e59a349ce8583c4aefc5b38
Ty Day <tyrday@gmail.com>
2018-09-29 21:38:49 -0400

    cleaned up code

--- static/index.js
+++ static/index.js
@@ -22,33 +22,31 @@ document.addEventListener('DOMContentLoaded', () => {
         // Test to see if announcement is in user chat room
         const chat_window = document.querySelector('#chat_window')
         let test_value = chat_window.dataset.room_id
-        console.log('current room:', test_value, 'emit room:',data.chat_room, (test_value == data.chat_room))
+        // console.log('current room:', test_value, 'emit room:',data.chat_room, (test_value == data.chat_room))
+        // if announcemnet in current room add it to ul
         if (test_value == data.chat_room){
             const li = document.createElement('li')
             const ul = document.querySelector('#chat_window_list')
             li.innerHTML = '<strong>'+ data['user_name']+'</strong> ' + data['chat_text']
-            
+            // adjust scroll position if user has scrolled to bottom
             if(ul.clientHeight + ul.scrollTop >= ul.scrollHeight){
                 ul.appendChild(li)
                 li.scrollIntoView()
             } else{
                 ul.appendChild(li)
             }
-        }
-        
+        }      
     })
+    // When user changes room... update main chat window with message list
     socket.on('update room', data => {
         const ul = document.querySelector('#chat_window_list')
         for (var i=0; i< data.chat_log.length;i++){
-        // for elem in data{
             const li = document.createElement('li');
             li.innerHTML = data.chat_log[i].user_name + ": " + data.chat_log[i].text;
             ul.appendChild(li);
             li.scrollIntoView();
         }
-    })
-
-    
+    })  
 })
 
 function submit_chat(x){
@@ -58,7 +56,7 @@ function submit_chat(x){
     x.emit('submit chat', {'chat_room':chat_room,'chat_text':chat_text})
     return false;
 }
-
+// Called when user clicks on new room
 function change_room(key, value){
     const ul = document.querySelector('#chat_window_list')
     const chat_window = document.querySelector('#chat_window')
@@ -66,21 +64,14 @@ function change_room(key, value){
     let room_destination = key;
     let room_source = chat_window.dataset.room_id;
     let data = {"room_source":room_source,"room_destination":room_destination}
-    console.log(`Room change. Source: ${room_source} - Dest: ${room_destination}`)
+    console.log(`Room change. Source: ${room_source} -> Dest: ${room_destination}`)
 
-    // Update UI
+    // Clear messages from message window and update message header
     chat_window.dataset.room_id = key
     while (ul.firstChild){
         ul.removeChild(ul.firstChild);
     const header = document.querySelector('#room_header')
     header.innerHTML = `Room: ${value['name']}`
     }
-    
-    socket.emit('submit room_change', data)
-    
-    
+    socket.emit('submit room_change', data)   
 }
-
-// user_name = data["user_name"]
-// room_source = data["room_source"]
-// room_destination = data["room_destination"]
